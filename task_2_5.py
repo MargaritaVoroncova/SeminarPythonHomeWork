f92d2c3e5df5ad26a9d146f0a1d68a1d015f9d1b
MargaritaVoroncova <margaritavrncv@gmail.com>
2022-10-03 14:23:23 +0300

    task_2_5

---
+++ task_2_5.py
@@ -0,0 +1,13 @@
+# 5. ** Реализуйте алгоритм перемешивания списка. 
+# Без функции shuffle из модуля random.
+
+import random
+
+array = [0, 1, 2 ,3 , 4, 5, 6, 7, 8, 9]
+print(array)
+for i in range(10):
+    temp = array[i]
+    index = random.randint(0,9)
+    array[i] = array[index]
+    array[index] = temp
+print(array)
